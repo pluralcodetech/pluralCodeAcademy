2372721994e2de6b53e006cfb5005a406a72948c
samuelige <samuelige.iyanu@gmail.com>
2021-06-17 12:47:37 +0100

    Changed Course List Data table to CRUD

--- src/views/Courses/course_lists.js
+++ src/views/Courses/course_lists.js
@@ -1,250 +1,123 @@
-import React, { useState, useEffect } from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
-import {
-  CBadge,
-  CCard,
-  CCardBody,
-  CCardHeader,
-  CCol,
-  CDataTable,
-  CRow,
-  CPagination
-} from '@coreui/react';
-
+import React, { forwardRef, useEffect } from 'react';
+import { AddBox, ArrowDownward } from "@material-ui/icons";
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import MaterialTable from 'material-table';
+import { Input } from "@material-ui/core";
+import { useDispatch, useSelector } from 'react-redux';
 import courseListAction from 'src/Redux Statement/actions/courseListAction';
+import { CCard, CCol, CRow } from '@coreui/react';
 
-import { useDispatch, useSelector } from 'react-redux'
-import usersData from '../users/UsersData';
-
-const getBadge = status => {
-    switch (status) {
-      case 'Active': return 'success'
-      case 'Inactive': return 'secondary'
-      case 'Pending': return 'warning'
-      case 'Banned': return 'danger'
-      default: return 'primary'
-    }
-}
-
-
-
-const CourseLists = () => {
+  const CourseLists = () => {
     const courseListContent = useSelector(state => state.courseListData.courseList);
-    // console.log();
-    const history = useHistory()
-    const queryPage = useLocation().search.match(/page=([0-9]+)/, '')
-    const currentPage = Number(queryPage && queryPage[1] ? queryPage[1] : 1)
-    const [page, setPage] = useState(currentPage)
-  
-    const pageChange = newPage => {
-      currentPage !== newPage && history.push(`/course_list?page=${newPage}`)
-    }
-    
+
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(courseListAction());
-      currentPage !== page && setPage(currentPage)
-    }, [currentPage, page])
+     
+    }, [])
 
+    const tableIcons = {
+        Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+        Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+        Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+        DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+        Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+        SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+        ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+        ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+    };
+
+    // Columns
     const columns = [
-        {datafield: 'name', text: 'Title'},
-        {datafield: 'description', text: 'Description'},
-        {datafield: 'price', text: 'Price'},
-        {datafield: 'start_date', text: 'State Date'},
-        {datafield: 'end_date', text: 'End Date'},
+        {title: 'Name', field: 'name'},
+        {title: 'Description', field: 'description'},
+        {title: 'Price', field: 'price'},
+        {title: 'Start Date', field: 'start_date'},
+        {title: 'End Date', field: 'end_date',
+            editComponent: editProps => (
+                <Input
+                    autoFocus={true}
+                    onChange={e => editProps.onChange(e.target.value)}
+                />
+            )
+        },
     ]
 
-
-    return (
+      return (
         <CRow>
             <CCol xl={12}>
                 <CCard>
-                <CCardHeader>
-                    Course Lists
-                </CCardHeader>
-                <CCardBody>
-                <CDataTable
-                    items={courseListContent}
-                    fields={[
-                    { key: 'name', _classes: 'font-weight-bold' },
-                     'description', 'price', 'start_date', 'end_date'
-                    ]}
-                    columnFilter
-                    tableFilter
-                    itemsPerpageSelect
-                    hover
-                    striped
-                    sorter
-                    itemsPerPage={5}
-                    activePage={page}
-                    clickableRows
-                    onRowClick={(item) => history.push(`/course_list/${item.id}`)}
-                    scopedSlots = {{
-                    'status':
-                        (item)=>(
-                        <td>
-                            <CBadge color={getBadge(item.status)}>
-                            {item.status}
-                            </CBadge>
-                        </td>
-                        )
+                    <MaterialTable
+                    icons={tableIcons}
+                    columns={columns}
+                    data = {courseListContent}
+                    title="Course List"
+                    options={{
+                        exportButton: true,
+                        
                     }}
-                />
-                <CPagination
-                    activePage={page}
-                    onActivePageChange={pageChange}
-                    pages={5}
-                    doubleArrows={true} 
-                    align="center"
-                />
-                </CCardBody>
+                    
+                    editable={{
+                        onRowAdd: newData =>
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                            //   setData([...data, newData]);
+            
+                            //   resolve();
+                            }, 1000);
+                        }),
+                        onRowUpdate: (newData, oldData) =>
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                            //   const dataUpdate = [...data];
+                            //   const index = oldData.tableData.id;
+                            //   dataUpdate[index] = newData;
+                            //   setData([...dataUpdate]);
+            
+                            resolve();
+                            }, 1000);
+                        }),
+                        onRowDelete: oldData =>
+                        new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                            //   const dataDelete = [...data];
+                            //   const index = oldData.tableData.id;
+                            //   dataDelete.splice(index, 1);
+                            //   setData([...dataDelete]);
+            
+                            resolve();
+                            }, 1000);
+                        })
+                    }}
+                    
+                    />
                 </CCard>
             </CCol>
-            </CRow>
+        </CRow>
 
-    
-    )
-}
-
-export default CourseLists
-
-
-// import React, { useEffect } from 'react'
-// import BootstrapTable from 'react-bootstrap-table-next';
-// import { useDispatch, useSelector } from 'react-redux';
-// import courseListAction from 'src/Redux Statement/actions/courseListAction';
-
-// const CourseLists = () => {
-//     const courseListContent = useSelector(state => state.courseListData.courseList);
-//     // const {image} = courseListContent
-//     console.log(JSON.parse(courseListContent.image));
-
-//     const dispatch = useDispatch();
-
-//     useEffect(() => {
-//         dispatch(courseListAction());
-//     }, []);
-
-    // const columns = [
-    //     {datafield: 'image', text: 'Image'},
-    //     {datafield: 'name', text: 'Title'},
-    //     {datafield: 'description', text: 'Description'},
-    //     {datafield: 'price', text: 'Price'},
-    //     {datafield: 'start_date', text: 'State Date'},
-    //     {datafield: 'end_date', text: 'End Date'},
-    // ]
-
-    // const data = [
-    //     {
-    //         "id": 1,
-    //         "image": "WEB Development",
-    //         "name": "WEB Development",
-    //         "description": "you get to learn to use react native, Laravel , Node js",
-    //         "price": "2500",
-    //         "start_date": "2021-06-09",
-    //         "end_date": "2021-06-16"
-    //     },
-    //     {
-    //         "id": 2,
-    //         "image": "IOT",
-    //         "name": "IOT",
-    //         "description": "Use themes to build beautiful websites",
-    //         "price": "45000",
-    //         "start_date": "2021-06-17",
-    //         "end_date": "2021-06-23"
-    //     },
-    //     {   
-    //         "id": 3,
-    //         "image": "blockchain",
-    //         "name": "blockchain",
-    //         "description": "learn crypto trading ",
-    //         "price": "45000",
-    //         "start_date": "2021-06-23",
-    //         "end_date": "2021-06-25"
-    //     },
-    //     {
-    //         "id": 3,
-    //         "image": "Degital marketing",
-    //         "name": "Degital marketing",
-    //         "description": "learn degital marketing ",
-    //         "price": "60000",
-    //         "start_date": "2021-06-27",
-    //         "end_date": "2021-06-29"
-    //     },
-    //     {
-    //         "id": 4,
-    //         "image": "WEB DEV starter class",
-    //         "name": "WEB DEV starter class",
-    //         "description": "learn dev",
-    //         "price": "2300",
-    //         "start_date": "0000-00-00",
-    //         "end_date": "0000-00-00"
-    //     },
-    //     {
-    //         "id": 5,
-    //         "image": "WEB DEV starter class",
-    //         "name": "WEB DEV starter class",
-    //         "description": "learn dev",
-    //         "price": "2300",
-    //         "start_date": "0000-00-00",
-    //         "end_date": "0000-00-00"
-    //     },
-    //     {
-    //         "id": 6,
-    //         "image": "WEB DEV class",
-    //         "name": "WEB DEV class",
-    //         "description": "learn dev",
-    //         "price": "2300",
-    //         "start_date": "0000-00-00",
-    //         "end_date": "0000-00-00"
-    //     },
-    //     {
-    //         "id": 6,
-    //         "image": "IOT sandbox",
-    //         "name": "IOT sandbox",
-    //         "description": "learcn codding with your felllow team members",
-    //         "price": "2500",
-    //         "start_date": "2021-06-23",
-    //         "end_date": "2021-06-23"
-    //     },
-    //     {
-    //         "id": 7,
-    //         "image": "IOT sandbox",
-    //         "name": "IOT sandbox",
-    //         "description": "learcn codding with your felllow team members",
-    //         "price": "2500",
-    //         "start_date": "2021-06-23",
-    //         "end_date": "2021-06-23"
-    //     },
-    //     {
-    //         "id": 8,
-    //         "image": "ui/ux",
-    //         "name": "ui/ux",
-    //         "description": "fine course",
-    //         "price": "2300",
-    //         "start_date": "2021-04-23",
-    //         "end_date": "2021-04-23"
-    //     },
-    //     {
-    //         "id": 9,
-    //         "image": "ui/ux",
-    //         "name": "ui/ux",
-    //         "description": "fine course",
-    //         "price": "2300",
-    //         "start_date": "2021-04-23",
-    //         "end_date": "2021-04-23"
-    //     }
-    // ]
-
-//     return (
-//         // <div className='overflow-scroll'>
-//         //     <BootstrapTable  keyField= 'id' columns={columns} sort={true} data={data} />
-//         // </div>
-
-        
-            
-//     )
-// }
-
-// export default CourseLists
+      )
+  }
+  
+  export default CourseLists
