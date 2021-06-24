60532ac8e26e43dc6608925d7301d0f1072ede3d
samuelige <samuelige.iyanu@gmail.com>
2021-06-24 10:10:54 +0100

    integrated course details API

---
+++ src/Redux Statement/actions/courseDetailsAction.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import COURSE_DETAILS from '../constants/courseDetails'
+
+const courseDetailsAction = (parseId) => async dispatch => {
+    dispatch ({
+        type: COURSE_DETAILS.POST_DETAILS_DATA
+    });
+
+    try {
+        const response = await axios({
+            method: "post",
+            url: 'http://codesandbox.com.ng/academyAPI/api/admincoursedetails.php',
+            data: parseId,
+            headers: { "Content-Type": "multipart/form-data" },
+        });
+
+        const {data} = response
+        console.log(typeof data);
+
+        dispatch({
+            type: COURSE_DETAILS.GET_DETAILS_SUCCESS,
+            payload: data
+        });
+    } catch (error) {
+        dispatch({
+            type: COURSE_DETAILS.GET_DETAILS_ERROR,
+            error
+        })
+    }
+};
+
+export default courseDetailsAction
--- src/Redux Statement/rootReducer.js
+++ src/Redux Statement/rootReducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
 import calendarDataReducer from "./reducers/calendarDataReducer";
+import courseDetailsReducer from "./reducers/courseDetailsReducer";
 import courseListReducer from "./reducers/courseListReducer";
 import sidebarShowReducer from "./reducers/sidebarShowReducer";
 
 const rootReducer = combineReducers({
     nav : sidebarShowReducer,
     courseListData : courseListReducer,
-    calendarData : calendarDataReducer
+    calendarData : calendarDataReducer,
+    courseDetailsData : courseDetailsReducer
 });
 
 export default rootReducer;
